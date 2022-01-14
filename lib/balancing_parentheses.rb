89da63dd901a9d3d6d8adb388533cac1ce08c15e
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2022-01-14 15:30:38 -0600

    commit tree bbde0bc1299429434dca553aafeeb7120a738362

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,21 @@
 require_relative './stack'
 
-# your code here
+def balancing_parentheses(string)
+    stack = Stack.new
+    chars_required = 0
+    left = 0
+    right = 0
+    first = 0
+    last = string.length
+    for i in first...last do
+        char = string[i]
+        if (i == first && char == ")") || (i == last && char == "(")
+            chars_required += 1
+        else
+            right += 1 if char == ")"
+            left += 1 if char == "("
+        end
+        stack.push char
+    end
+    chars_required += (left - right).abs
+end
